081892622be9e989ab375a9f86bf39f2565f0946
Mostafa Nafie <MostafaTarekNafie@gmail.com>
2025-08-27 10:21:23 +0300

    Initial Commit

---
+++ ABTestingDemo/ABTestingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABTestingDemoApp.swift
+//  ABTestingDemo
+//
+//  Created by Nafie on 27/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ABTestingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
